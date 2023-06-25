278323dbff9bf4d3ebb05761c456a7c8262d19df
Abhishek Raturi <abhishek.raturi6@gmail.com>
2023-06-25 13:25:57 +0530

    auto build

--- app.py
+++ app.py
@@ -1,9 +1,2 @@
-from flask import Flask
-app = Flask(__name__)
-
-@app.route('/')
-def hello_world():
-    return 'Hello, Docker! This is version 1'
+print('Hello, jenkins! This is automatic trigger')
     
-if __name__ == "__main__":
-    app.run(host='13.232.56.25', port=5000)
